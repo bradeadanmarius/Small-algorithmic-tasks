c07f2f78f12214c8a73ce403a74bd232dfe44e21
bradeadanmarius <bradeadanmarius@gmail.com>
2020-12-28 12:28:42 +0000

    Create Compare a given number with digits of a number.js

---
+++ Compare a given number with digits of a number.js
@@ -0,0 +1,20 @@
+// Checking if the digits of a number are smaller or equal to the given number
+
+const gNumber = 3
+let number = 123
+
+function smallerOrEqual (number, gNumber){
+    while (number>0){
+        lastDigit = number%10
+        number = (number - number%10)/10
+        console.log (number)
+    if (lastDigit <= gNumber) {
+        // return true 
+    } else {
+        return false
+    }
+    }
+    return true
+}
+
+console.log (smallerOrEqual (number,gNumber))

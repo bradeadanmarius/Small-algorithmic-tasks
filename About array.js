7a97c036140794a2c251dd3f7d73f2720a0d3539
bradeadanmarius <bradeadanmarius@gmail.com>
2021-01-13 18:58:21 +0000

    Create About array.js

---
+++ About array.js
@@ -0,0 +1,8 @@
+array = ["ion", 25, "carnati", "varza", "sarac", "lipit"]
+
+for (index = 0; index <= (array.length - 1); index++) {
+    if (index % 2 == 0) {
+        //console.log(array[index])
+    }
+    console.log(array[index])
+}
